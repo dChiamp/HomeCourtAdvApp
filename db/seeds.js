1ac2ef1b278681fc799ba85dc20571a225f85219
howellwho <howellwho@gmail.com>
2016-03-12 20:47:48 -0800

    adds event model and seed data

--- db/seeds.js
+++ db/seeds.js
@@ -1,16 +1,68 @@
 var mongoose = require('mongoose'),
         conn = mongoose.connect('mongodb://localhost/HCA-app'),
         Team = require("../models/team");
+        Event = require("../models/event");
 
 Team.remove({}, function(err){
   if (err) console.log("ERROR: ", err);
 });
 
+
+Event.remove({}, function(err){
+  if (err) console.log("ERROR: ", err);
+});
+
+var events = [
+  {
+  homeTeam: "Bulls",
+  awayTeam: "Jazz",
+  date: "3/19",
+  time: "5:00 pm PST"
+  },
+  {
+  homeTeam: "Bulls",
+  awayTeam: "Kings",
+  date: "3/21",
+  time: "5:00 pm PST"
+  },
+  {
+  homeTeam: "Bulls",
+  awayTeam: "Knicks",
+  date: "3/23",
+  time: "5:00 pm PST"
+  },
+  {
+  homeTeam: "Knicks",
+  awayTeam: "Bulls",
+  date: "3/24",
+  time: "4:30 pm PST"
+  },
+  {
+  homeTeam: "Magic",
+  awayTeam: "Bulls",
+  date: "3/26",
+  time: "4:00 pm PST"
+  },
+  {
+  homeTeam: "Bulls",
+  awayTeam: "Hawks",
+  date: "3/28",
+  time: "5:00 pm PST"
+  },
+  {
+  homeTeam: "Hornets",
+  awayTeam: "Nuggets",
+  date: "3/19",
+  time: "3:00 pm PST"
+  }
+];
+
+
 var teams = [
   {
-  name: "Bulls", 
-  league: "NBA", 
-  origin: "Chicago", 
+  name: "Bulls",
+  league: "NBA",
+  origin: "Chicago",
   logo: "http://content.sportslogos.net/logos/6/221/thumbs/hj3gmh82w9hffmeh3fjm5h874.gif"
   },
   {
@@ -27,11 +79,20 @@ var teams = [
   }
 ];
 
+Event.create(events, function(err, events){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("create: ", events);
+  }
+});
+
 Team.create(teams, function(err, teams){
   if (err) {
     console.log(err);
   } else {
-    console.log("created: ", teams); 
+    console.log("created: ", teams);
     mongoose.connection.close();
-  } 
+  }
+
 });
