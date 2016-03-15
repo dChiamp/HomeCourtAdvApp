06660710cb6d0583ebba608ae12951f5a3190d2b
dchiamp <derek.chiampas@gmail.com>
2016-03-15 13:22:29 -0700

    adds multiple pins on map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -4,19 +4,25 @@
 var barEndpoint = "/bars"
 
 function getBarInfo () {
-  $.getJSON(barEndpoint, function(data){
-    bars = data;
-    for (var key in data) {
-      console.log(data[key][0].lat)
+  $.getJSON(barEndpoint, function(bars){
+    bars = bars;
+    // get all objects in bar db
+    for (var bar in bars) {
+      // get each bar lat/long
+      for (i=0; i < bars[bar].length; i++) {
+      console.log(bars[bar][i].lat)
+      barLat = bars[bar][i].lat;
+      barLong = bars[bar][i].long;
+      console.log(barLat);
+      // barCity = bars.bar[0].city;
+
+      // render each bar as pin on map
+        new google.maps.Marker({
+          position: new google.maps.LatLng(barLat, barLong),
+          map: map,
+        });
+      }
     }
-     barLat = data.bars[0].lat;
-     barLong = data.bars[0].long;
-     // barCity = data.bar[0].city;
-    new google.maps.Marker({
-      position: new google.maps.LatLng(barLat, barLong),
-      map: map,
-      // title: title
-    });
   })
 }
 
